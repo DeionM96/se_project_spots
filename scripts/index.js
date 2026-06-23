e581029103a1b7c2c4e9d4d1760e6baeed64db28
Deion Mitchell <deionmitchell15@gmail.com>
2026-06-23 15:28:38 -0400

    fix typos in html/blocks/profile.css,blocks/modal, and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 console.log("Hello, World");
 //make the edit profile button open the edit profile form and make the x-icon remove the modal_is-open.
 //create the varible using the const = document.querySelector(name the class);
-const ediProfileBtn = document.querySelector(".profile__edi-button");
+const editProfileBtn = document.querySelector(".profile__edit-button");
 //create another varible using the const = document.qerySelector(use a # to identify the id);
 const editProfileModal = document.querySelector("#edit-profile-modal");
 //create another varible were editProfileModal is neated inside of the close button varible.
@@ -16,18 +16,18 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 
 //for the profile edit button - add an eventListeren with the "click" function ()
-ediProfileBtn.eventListener("click", function () {
-  editProfileModal.classlist.add("modal_is-opened");
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
 });
 //this bit removes the visibility of the modal aka removes the modal modal_is-open in HTML.
-editProfileCloseBtn.eventListener("click", function () {
+editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 //this bit is connected to the new post button.
-newPostBtn.eventListener("click", function () {
+newPostBtn.addEventListener("click", function () {
   //this bit opens the modal in the same way we use modal modal_is-open in HTML.
-  newPostModal.classlist.add("modal_is-opened");
+  newPostModal.classList.add("modal_is-opened");
 });
-newPostCloseBtn.eventListener("click", function () {
+newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
